c33ebb566d69aaa3dbc832be818f7f49b23e4a02
Eric Juquel <eric.juquel@ratp.fr>
2026-02-25 16:13:02 +0100

    feat: skills page layout and paragraphes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { APP_ROUTES } from "./modules/core/routes";
 import HomeView from "./modules/home/HomeView";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import NotFound from "./modules/core/error/NotFound";
+import SkillsView from "./modules/skills/SkillsView";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to={APP_ROUTES.home.href} />} />
         <Route path={`${APP_ROUTES.home.href}/*`} element={<HomeView />} />
+        <Route path={`${APP_ROUTES.skills.href}/*`} element={<SkillsView />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </Layout>

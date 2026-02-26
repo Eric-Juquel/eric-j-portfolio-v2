c5d75fe630356ffbaf7b9211502258f75e22ecd1
Eric Juquel <eric.juquel@ratp.fr>
2026-02-26 17:22:48 +0100

    feat: add professional section

---
+++ src/modules/achievements/professionnal/WoodySVG.tsx
@@ -0,0 +1,45 @@
+import type { SVGProps } from "react";
+
+interface SvgComponentProps extends SVGProps<SVGSVGElement> {}
+
+export default function WoodySVG(props: SvgComponentProps) {
+  return (
+    <svg
+      id="woody-technology-pink-logo_svg__Calque_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x={0}
+      y={0}
+      viewBox="0 0 271 54.3"
+      xmlSpace="preserve"
+      {...props}
+    >
+      <title>Woody Technology Pink Logo</title>
+      <style>
+        {
+          ".woody-technology-pink-logo_svg__st1{clip-path:url(#woody-technology-pink-logo_svg__SVGID_2_);fill:#e50071}"
+        }
+      </style>
+      <path
+        fill="#e50071"
+        d="M16.1 49.6L1.3 8.9h4.9l11.9 32.7L30 8.9h5.5l11.9 32.7L59.2 8.9h5L49.4 49.6h-4L32.7 14.8 20.1 49.6z"
+      />
+      <defs>
+        <path
+          id="woody-technology-pink-logo_svg__SVGID_1_"
+          d="M-285.8-226.6h841.9v595.3h-841.9z"
+        />
+      </defs>
+      <clipPath id="woody-technology-pink-logo_svg__SVGID_2_">
+        <use
+          xlinkHref="#woody-technology-pink-logo_svg__SVGID_1_"
+          overflow="visible"
+        />
+      </clipPath>
+      <path
+        className="woody-technology-pink-logo_svg__st1"
+        d="M79.6 49.6c-1.9 0-3.5-.7-4.8-2-1.3-1.3-2-2.9-2-4.8V15.7c0-1.9.7-3.5 2-4.8 1.3-1.3 2.9-2 4.8-2h27.1c1.9 0 3.5.7 4.8 2 1.3 1.3 2 2.9 2 4.8v27.1c0 1.9-.7 3.5-2 4.8-1.3 1.3-2.9 2-4.8 2H79.6zm0-4.6h27.1c.6 0 1.1-.2 1.6-.6.4-.4.6-1 .6-1.6V15.7c0-.6-.2-1.1-.6-1.6-.4-.4-1-.6-1.6-.6H79.6c-.6 0-1.1.2-1.6.6-.4.4-.6 1-.6 1.6v27.1c0 .6.2 1.1.6 1.6.5.4 1 .6 1.6.6M131.5 49.6c-1.9 0-3.5-.7-4.8-2-1.3-1.3-2-2.9-2-4.8V15.7c0-1.9.7-3.5 2-4.8 1.3-1.3 2.9-2 4.8-2h27.1c1.9 0 3.5.7 4.8 2 1.3 1.3 2 2.9 2 4.8v27.1c0 1.9-.7 3.5-2 4.8-1.3 1.3-2.9 2-4.8 2h-27.1zm0-4.6h27.1c.6 0 1.1-.2 1.6-.6.4-.4.6-1 .6-1.6V15.7c0-.6-.2-1.1-.6-1.6-.4-.4-.9-.6-1.6-.6h-27.1c-.6 0-1.1.2-1.6.6-.4.4-.6 1-.6 1.6v27.1c0 .6.2 1.1.6 1.6.5.4 1 .6 1.6.6M244.8 49.6V34.3L226.1 8.9h5.3l15.7 19.9 15.6-19.9h5.4l-18.7 25.4v15.3zM176.8 15.7v27.1c0 1.9.7 3.5 2 4.8 1.3 1.3 2.9 2 4.8 2h33.9V1h-4.6v7.9h-29.3c-1.9 0-3.5.7-4.8 2-1.3 1.3-2 2.9-2 4.8m4.6 0c0-.6.2-1.1.6-1.6.4-.4 1-.6 1.6-.6h27.1c.6 0 1.1.2 1.6.6.4.4.6 1 .6 1.6v27.1c0 .6-.2 1.1-.6 1.6-.4.4-1 .6-1.6.6h-27.1c-.6 0-1.1-.2-1.6-.6-.4-.4-.6-.9-.6-1.6V15.7z"
+      />
+    </svg>
+  );
+}
--- src/modules/home/HomeView.tsx
+++ src/modules/home/HomeView.tsx
@@ -1,22 +1,20 @@
 import { useTranslation } from "react-i18next";
-import ButtonWave from "../shared/components/buttons/ButtonWave";
+import ButtonWave from "@/shared/components/buttons/ButtonWave";
 
 export default function HomeView() {
   const { t } = useTranslation();
   return (
     <div className="flex flex-col justify-center w-full">
       <div className="flex flex-col align-center w-100 md:flex-row md:w-200 ">
-        <div className="grid h-50  w-1/3 [grid-template-columns:repeat(5,3rem)] [grid-template-rows:repeat(5,3rem)] text-5xl font-capriola">
-          <div className="[grid-column:1/3] [grid-row:1/span_3] -ml-[0.8rem] mt-5 text-[7rem] bg-gradient-to-b from-title-first-from to-title-first-to bg-clip-text text-transparent">
+        <div className="grid h-50  w-1/3 grid-cols-[repeat(5,3rem)] grid-rows-[repeat(5,3rem)] text-5xl font-capriola">
+          <div className="col-[1/3] row-[1/span_3] -ml-[0.8rem] mt-5 text-[7rem] bg-linear-to-b from-title-first-from to-title-first-to bg-clip-text text-transparent">
             E
           </div>
-          <div className="[grid-column:2/4] [grid-row:2/span_1] -ml-[1.5rem] text-center">
-            ric
-          </div>
-          <div className="[grid-column:2/4] [grid-row:3/span_3] -mt-[0.8rem] text-[7rem] bg-gradient-to-b from-title-last-from to-title-last-to bg-clip-text text-transparent ">
+          <div className="col-[2/4] row-[2/span_1] -ml-6 text-center">ric</div>
+          <div className="col-[2/4] row-[3/span_3] -mt-[0.8rem] text-[7rem] bg-linear-to-b from-title-last-from to-title-last-to bg-clip-text text-transparent ">
             J
           </div>
-          <div className="[grid-column:3/5] [grid-row:3/span_1] ">uquel</div>
+          <div className="col-[3/5] row-[3/span_1] ">uquel</div>
         </div>
         <div className="flex flex-col justify-center">
           <h4>{t("title")}</h4>

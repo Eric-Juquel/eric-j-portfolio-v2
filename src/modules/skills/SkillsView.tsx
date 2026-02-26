f9f7de86d8da8b0a88babc8c760c9bab158a42f0
Eric Juquel <eric.juquel@ratp.fr>
2026-02-26 11:22:01 +0100

    feat: skill page complete

--- src/modules/skills/SkillsView.tsx
+++ src/modules/skills/SkillsView.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import ChipBox from "./ChipBox";
+import SkillParagraph from "./components/SkillParagraph";
 import ProgressBox from "./ProgressBox";
-import SkillParagraph from "./SkillParagraph";
 import { list_1, list_2, list_3, list_4 } from "./skills.const";
 
 export default function SkillsView() {
@@ -11,36 +11,36 @@ export default function SkillsView() {
       <div className="h-33 w-fit">
         <h4 className="text-6xl">{t("skills")}</h4>
       </div>
-      <div className="grid grid-cols-1 gap-6 mb-10 lg:grid-cols-2 lg:grid-rows-4 lg:gap-10 lg:mb-36">
+      <div className="grid grid-cols-1 gap-6 mb-10 lg:grid-cols-2  ">
         <SkillParagraph
           label={t("skills-1")}
           list={list_1}
-          className="order-2 lg:col-start-1 lg:row-start-1 hidden min-[440px]:block"
+          className="order-2 lg:col-start-1 lg:row-start-1 min-[440px]:block self-center"
         />
 
         <SkillParagraph
           label={t("skills-2")}
           list={list_2}
-          className="order-3 lg:order-none lg:col-start-1 lg:row-start-2"
+          className="order-3 lg:order-0 lg:col-start-1 lg:row-start-2 self-center "
         />
 
         <SkillParagraph
           label={t("skills-3")}
           list={list_3}
-          className="order-5 lg:order-none lg:col-start-1 lg:row-start-3"
+          className="order-5 lg:order-0 lg:col-start-1 lg:row-start-4 self-center"
         />
 
         <SkillParagraph
           label={t("skills-4")}
           list={list_4}
-          className="order-6 lg:order-none lg:col-start-1 lg:row-start-4"
+          className="order-6 lg:order-0 lg:col-start-1 lg:row-start-3 self-center"
         />
 
-        <div className="order-1 lg:order-none lg:col-start-2 lg:row-start-1 lg:row-span-2 self-start">
+        <div className="order-1 lg:order-0 lg:col-start-2 lg:row-start-1 lg:row-span-3 ">
           <ProgressBox />
         </div>
 
-        <div className="order-4 lg:order-none lg:col-start-2 lg:row-start-3 ">
+        <div className="order-4 lg:order-0 lg:col-start-2 lg:row-start-4 ">
           <ChipBox />
         </div>
       </div>
